f1a785eb1825cc180fbec25cfe9ce482857afdff
T0nci <138928016+T0nci@users.noreply.github.com>
2024-05-03 12:48:16 +0200

    Write and test capitalize function

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,3 +1,5 @@
-function capitalize() {}
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export default capitalize;
--- src/tests/capitalize.test.js
+++ src/tests/capitalize.test.js
@@ -1,5 +1,19 @@
 import capitalize from "../capitalize";
 
-test("Test", () => {
-  expect(capitalize()).toBe(undefined);
+test("capitalize function capitalizes first letter", () => {
+  expect(capitalize("jest")).toBe("Jest");
+  expect(capitalize("modules")).toBe("Modules");
+  expect(capitalize("babel")).toBe("Babel");
+});
+
+test("capitalize function returns same string if already capitalized", () => {
+  expect(capitalize("Jest")).toBe("Jest");
+  expect(capitalize("Modules")).toBe("Modules");
+  expect(capitalize("Babel")).toBe("Babel");
+});
+
+test("capitalize function only changes the first letter", () => {
+  expect(capitalize("jEST")).toBe("JEST");
+  expect(capitalize("mODules")).toBe("MODules");
+  expect(capitalize("bABEL")).toBe("BABEL");
 });
